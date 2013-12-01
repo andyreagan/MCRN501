402afbe57d48425db752e6c9ec280bcf7c925937
Andy Reagan <github@andyreagan.com>
2013-11-30 20:00:17 -0500

    experiment running now

--- project003/UVM/DA-code/runDAParamFitAll.sh
+++ project003/UVM/DA-code/runDAParamFitAll.sh
@@ -4,37 +4,53 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/gpfs1/arch/x86_64/matlab2010a/bin/glnxa64:/gpfs1/arch/x86_64/matlab2010a/runtime/glnxa64:/gpfs1/arch/x86_64/matlab2010a/sys/os/glnxa64:/gpfs1/arch/x86_64/matlab2010a/sys/java/jre/glnxa64/jre/lib/amd64/native_threads:/gpfs1/arch/x86_64/matlab2010a/sys/java/jre/glnxa64/jre/lib/amd64/server:/gpfs1/arch/x86_64/matlab2010a/sys/java/jre/glnxa64/jre/lib/amd64/client:/gpfs1/arch/x86_64/matlab2010a/sys/java/jre/glnxa64/jre/lib/amd64
 export XAPPLRESDIR=/gpfs1/arch/x86_64/matlab2010a/X11/app-defaults
 
-cd /users/a/r/areagan/work/2013/data-assimilation/src/experiments/openFoamAssimilation
-
-# C=40
-# SEED=0
-# for B in 2 4 8 16 32 
-# do
-#   for D in 10 20 30
-#   do
-#     for E in 2 3 5 10 20
-#     do
-#       echo $B $C $D $E
-#       qsub -v NUMTSENSORS="$B",NUMWINDOWS="$C",WINDOWLEN="$D",NUMENS="$E",COUNTER="$SEED" runFullModelLocalZones.qsub
-#       SEED=$(($SEED+1))
-#     done
-#   done
-# done
+cd /users/a/r/areagan/class/2013/MCRN501/project003/UVM/DA-code
 
 ## for a single test (always find something wrong)
-EXPCOUNT=1
+EXPCOUNT=1 ## will run through 5 experiments
 ## tunable parameters
-NUMRUNS=1
+NUMRUNS=100
 RUNTIME=200
-
 ## part of experiment
 OBSERROR=0.05
-ERRORDIST="normal"
+ERRORDIST="normal" #"uniform"
 SUBSAMPLEALPHA=1
 RHO=28
 OBSVAR=3
 qsub -v NUMRUNS,RUNTIME,OBSERROR,ERRORDIST,SUBSAMPLEALPHA,RHO,OBSVAR,EXPCOUNT run.qsub
 
-
-
-
+## tunable parameters
+NUMRUNS=100
+RUNTIME=200
+## part of experiment
+RHO=28
+## for normal error
+for EXPCOUNT in {1..5} ## will run through 5 experiments
+do
+  ERRORDIST="normal"
+  for OBSERROR in 0 0.01 0.05 0.1 0.25 0.5 1 2
+  do
+    for SUBSAMPLEALPHA in 1 5 25 50
+    do
+      for OBSVAR in 1 3
+      do
+	qsub -v NUMRUNS,RUNTIME,OBSERROR,ERRORDIST,SUBSAMPLEALPHA,RHO,OBSVAR,EXPCOUNT run.qsub
+      done
+    done
+  done
+done
+## for uniform error
+for EXPCOUNT in {1..5} ## will run through 5 experiments
+do
+  ERRORDIST="uniform"
+  for OBSERROR in 0 0.5 2 4 6 8 10
+  do
+    for SUBSAMPLEALPHA in 1 5 25 50
+    do
+      for OBSVAR in 1 3
+      do
+	qsub -v NUMRUNS,RUNTIME,OBSERROR,ERRORDIST,SUBSAMPLEALPHA,RHO,OBSVAR,EXPCOUNT run.qsub
+      done
+    done
+  done
+done
--- project003/UVM/DA-code/run_filter.m
+++ project003/UVM/DA-code/run_filter.m
@@ -20,11 +20,12 @@ runTime = checkenv('RUNTIME',50); %  model units
 
 %% part of experiment
 obs_error_std = checkenv('OBSERROR',0.05);
-obs_error_type = checkenv('OBSERRORDIST','normal');
+obs_error_type = checkenv('ERRORDIST','normal');
 windowAlpha = checkenv('SUBSAMPLEALPHA',1); 
 rho = checkenv('RHO',28);
 obsVarLen = checkenv('OBSVAR',3);
 obsVar = 1:obsVarLen;
+expCount = checkenv('EXPCOUNT',1);
 
 %% set things based off of this
 windowLen = windowAlpha*tStep;
@@ -38,7 +39,9 @@ R = obs_error_std*eye(length(obsVar));
 
 %% initialize storage
 num_windows = ceil(runTime/windowLen);
-truth_vec = repmat(zeros(dim,num_windows+1),1,numRuns);
+%%truth_vec = repmat(zeros(dim,num_windows+1),1,numRuns);
+%% meaning of numRuns is not bootstrapping, but sampling IC now
+truth_vec = ones(dim,num_windows+1);
 
 %%  make a truth run
 truthmodel = lorenz63();
@@ -46,6 +49,7 @@ truthmodel.params = params;
 truthmodel.init();
 truthmodel.window = windowLen;
 %% save the IC
+tmp_truth_vec = ones(3,num_windows+1);
 tmp_truth_vec(1:3,1) = truthmodel.x;
 %% generate the truth x timeseries
 for j=1:num_windows
@@ -53,7 +57,9 @@ for j=1:num_windows
 	tmp_truth_vec(1:3,j+1) = truthmodel.x;
 end
 truth_vec(1:3,1) = tmp_truth_vec(1:3,1);
-truth_vec(1:3,2:end) = repmat(tmp_truth_vec(:,2:end),1,numRuns);
+%% truth_vec(1:3,2:end) = repmat(tmp_truth_vec(:,2:end),1,numRuns);
+%% numRuns is now just the number of IC samples, not bootstrapping
+truth_vec(1:3,2:end) = tmp_truth_vec(:,2:end);
 %% fill in the truth parameters
 truth_vec(4:6,:) = repmat([params{1};params{2};params{3}],1,length(truth_vec(1,:)));
 
@@ -65,40 +71,36 @@ switch obs_error_type
     obs_pert = truth_vec(obsVar,:) + obs_error_std*rand(size(truth_vec(obsVar,:))) - obs_error_std/2*ones(size(truth_vec(obsVar,:)));
 end
 
-[EKF_f_vec,EKF_a_vec] = modelDAinterface(@lorenz63_paramEst,'EKF',obs_pert,0:windowLen:(runTime*numRuns),H,R,windowLen,0,0);
+%% for running one of them, looking at the results
+%% [EKF_f_vec,EKF_a_vec] = modelDAinterface(@lorenz63_paramEst,'EKF',obs_pert,0:windowLen:(runTime*numRuns),H,R,windowLen,0,0);
 %% [ETKF_f_vec,ETKF_a_vec] = modelDAinterface(@lorenz63_paramEst,'ETKF',obs_pert,0:windowLen:(runTime*numRuns),H,R,windowLen,0,0); 
-fprintf('initial parameters\n')
-disp(EKF_f_vec(:,1))
 
-fprintf('end of the truth\n')
-disp(truth_vec(:,end-10:end))
+%% fprintf('initial parameters\n'); disp(EKF_f_vec(:,1))
+%% fprintf('end of the truth\n'); disp(truth_vec(:,end-10:end))
+%% fprintf('end of the EKF forecast\n'); disp(EKF_f_vec(:,end-10:end))
+%% fprintf('end of the ETKF forecast\n'); disp(ETKF_f_vec(:,end-10:end))
+%% fprintf('forecast errors of EKF\n'); disp(std(EKF_f_vec(1,floor(length(EKF_f_vec(1,:))/2):end)-truth_vec(1,floor(length(EKF_f_vec(1,:))/2):end)))
+%% fprintf('analysis errors of EKF\n'); disp(std(EKF_a_vec(1,floor(length(EKF_a_vec(1,:))/2):end)-truth_vec(1,floor(length(EKF_a_vec(1,:))/2):end)))
+%% fprintf('forecast errors of ETKF\n'); disp(std(ETKF_f_vec(1,floor(length(ETKF_f_vec(1,:))/2):end)-truth_vec(1,floor(length(ETKF_f_vec(1,:))/2):end)))
+%% fprintf('analysis errors of ETKF\n'); disp(std(ETKF_a_vec(1,floor(length(ETKF_a_vec(1,:))/2):end)-truth_vec(1,floor(length(ETKF_a_vec(1,:))/2):end)))
 
-fprintf('end of the EKF forecast\n')
-disp(EKF_f_vec(:,end-10:end))
+%% store many runs (this will be output). first entry is truth
+allForecasts = ones(dim,numRuns+1);
+allForecasts(:,1) = truth_vec(:,end);
+allAnalyses = allForecasts;
 
-fprintf('end of the ETKF forecast\n')
-disp(ETKF_f_vec(:,end-10:end))
-
-fprintf('forecast errors of EKF\n')
-disp(std(EKF_f_vec(1,floor(length(EKF_f_vec(1,:))/2):end)-truth_vec(1,floor(length(EKF_f_vec(1,:))/2):end)))
-
-fprintf('analysis errors of EKF\n')
-disp(std(EKF_a_vec(1,floor(length(EKF_a_vec(1,:))/2):end)-truth_vec(1,floor(length(EKF_a_vec(1,:))/2):end)))
-
-fprintf('forecast errors of ETKF\n')
-disp(std(ETKF_f_vec(1,floor(length(ETKF_f_vec(1,:))/2):end)-truth_vec(1,floor(length(ETKF_f_vec(1,:))/2):end)))
-
-fprintf('analysis errors of ETKF\n')
-disp(std(ETKF_a_vec(1,floor(length(ETKF_a_vec(1,:))/2):end)-truth_vec(1,floor(length(ETKF_a_vec(1,:))/2):end)))
-
-
-
-% compute the running RMS error of each of those forecasts
-for j=1:num_windows+1
-	% for each variable
-	for k=1:dim
-		%errors_DA(k,j) = errors_DA(k,j)+rmse(truth_vec(k,1:j),ETKF_f_vec(k,1:j));
-		%errors_direct(k,j) = errors_direct(k,j)+rmse(truth_vec(k,1:j),direct_f_vec(k,1:j));
-		%errors_none(k,j) = errors_none(k,j)+rmse(truth_vec(k,1:j),none_f_vec(k,1:j));
-	end
+%% now run a lot of them
+for i=1:numRuns
+  [tmp_EKF_f_vec,tmp_EKF_a_vec] = modelDAinterface(@lorenz63_paramEst,'EKF',obs_pert,0:windowLen:runTime,H,R,windowLen,0,0);
+  allForecasts(:,i+1) = tmp_EKF_f_vec(:,end);
+  allAnalyses(:,i+1) = tmp_EKF_a_vec(:,end);
 end
+
+%% save all of the end
+%% forecasts
+csvwrite(sprintf('data/%s_%g_%g_%g_%g_%g_%g_%g_forecastEnds.csv',obs_error_type,obs_error_std,numRuns,runTime,windowAlpha,rho,obsVarLen,expCount),allForecasts);
+csvwrite(sprintf('data/%s_%g_%g_%g_%g_%g_%g_%g_analysisEnds.csv',obs_error_type,obs_error_std,numRuns,runTime,windowAlpha,rho,obsVarLen,expCount),allAnalyses);
+
+
+
+
